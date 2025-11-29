5de4bda66bcfd92e265727b62c827e148acc0ae7
FatimaBalbuena <fatimalbalbuena@gmail.com>
2025-11-28 20:43:35 -0600

    Implementacion funcion resta

--- src/operations/resta.py
+++ src/operations/resta.py
@@ -1,2 +1,13 @@
-def resta(a, b):
-    pass
+ddef restar(x: float, y: float) -> float:
+    """
+    Calcula la resta de dos números.
+
+    Args:
+
+        x: El primer número (flotante).
+        y: El segundo número (flotante).
+
+    Returns:
+        La resta de x y y.
+    """
+    return x - y
