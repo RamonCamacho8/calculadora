d1ff6f5b71f5a6a4b7e7e64405ce72e7595cfe3d
ramon.cs@culiacan.tecnm.mx <113393688+DevArce@users.noreply.github.com>
2025-11-28 19:57:13 -0700

    Solucionado importaciones

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from operations.suma import suma
-from operations.resta import resta
-from operations.multiplicacion import multiplicacion
-from operations.division import division
+from src.operations.suma import sumar
+from src.operations.resta import restar
+from src.operations.multiplicacion import multiplicar
+from src.operations.division import division
 
 if __name__ == "__main__":
 
     a, b = 10, 5
-    print("suma:", suma(a, b))
-    print("resta:", resta(a, b))
-    print("multiplicacion:", multiplicacion(a, b))
+    print("suma:", sumar(a, b))
+    print("resta:", restar(a, b))
+    print("multiplicacion:", multiplicar(a, b))
     print("division:", division(a, b))
