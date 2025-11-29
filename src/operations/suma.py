f088b860a71208909609f5de9479e2f01cd4d2a6
FatimaBalbuena <fatimalbalbuena@gmail.com>
2025-11-28 20:29:10 -0600

    Implemente la funcion de suma

--- src/operations/suma.py
+++ src/operations/suma.py
@@ -1,2 +1,13 @@
-def suma(a, b):
-    pass
+def sumar(x: float, y: float) -> float:
+    """
+    Calcula la suma de dos números.
+
+    Args:
+
+        x: El primer número (flotante).
+        y: El segundo número (flotante).
+
+    Returns:
+        La suma de x y y.
+    """
+    return x + y
